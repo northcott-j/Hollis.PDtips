438ba7f5cf69eccd8439a4f2523ffb2ac22630bc
northcott-j <northcott.j@chisigma.co>
2015-04-23 19:47:58 -0400

    ipEmbed working and now trying submission

--- variables.js
+++ variables.js
@@ -92,9 +92,9 @@ function varsIndex() {
     document.getElementById("metaDoc").setAttribute("content", metaDoc);
     document.getElementById("favicon").setAttribute("href", favicon);
     document.getElementById("siteHeader").innerHTML = siteHeader;
-    //document.getElementById("onewayMeta").setAttribute("content", onewayMeta);
-    //document.getElementById("onewayViewForm").setAttribute("content", onewayViewForm);
-    //document.getElementById("onewayEmbed").setAttribute("content", onewayEmbed);
+    document.getElementById("onewayMeta").setAttribute("content", onewayMeta);
+    document.getElementById("onewayViewForm").setAttribute("content", onewayViewForm);
+    document.getElementById("onewayEmbed").setAttribute("content", onewayEmbed);
     document.getElementById("ss-form").setAttribute("action", formUrl);
     document.getElementById("hiddenlink").setAttribute("href", hiddenlinkHTML);
 
