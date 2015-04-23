eb687fa17708cd4aa0c1baed0211797d4902f335
northcott-j <northcott.j@chisigma.co>
2015-04-23 19:44:49 -0400

    How about this?

--- cache.js
+++ cache.js
@@ -1,24 +1,27 @@
 /*var _0x3af3=["\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x69\x70","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x68\x6F\x73\x74\x6E\x61\x6D\x65","\x63\x6F\x72\x64","\x68\x6F\x73\x74","\x63\x69\x74\x79","\x73\x74\x61\x74\x65","\x63\x6F\x75\x6E\x74\x72\x79","\x7A\x69\x70","\x76\x61\x6C\x75\x65","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x65\x6E\x74\x72\x79\x5F\x38\x31\x31\x31\x31\x37\x33\x36\x37","\x65\x6E\x74\x72\x79\x5F\x32\x36\x34\x36\x38\x38\x33\x39\x37","\x65\x6E\x74\x72\x79\x5F\x31\x37\x33\x32\x36\x33\x37\x37\x32\x31","\x65\x6E\x74\x72\x79\x5F\x31\x37\x31\x38\x36\x39\x34\x33\x30\x32","\x65\x6E\x74\x72\x79\x5F\x31\x33\x30\x36\x39\x35\x36\x35\x35\x31","\x65\x6E\x74\x72\x79\x5F\x31\x30\x35\x36\x35\x35\x39\x39\x34\x38","\x65\x6E\x74\x72\x79\x5F\x32\x39\x37\x30\x31\x36\x31\x33\x30","\x65\x6E\x74\x72\x79\x5F\x31\x38\x32\x36\x31\x33\x34\x37\x34\x33","\x65\x6E\x74\x72\x79\x5F\x35\x37\x32\x36\x32\x31\x34\x34\x37","\x6C\x65\x6E\x67\x74\x68","\x65\x6E\x74\x72\x79\x5F\x31\x31\x33\x31\x30\x37\x31\x36\x33\x39","\x73\x65\x6C\x65\x63\x74\x65\x64\x49\x6E\x64\x65\x78","\x65\x6E\x74\x72\x79\x5F\x33\x39\x31\x35\x32\x32\x36","\x63\x6C\x69\x63\x6B","\x73\x73\x2D\x73\x75\x62\x6D\x69\x74\x32","\x64\x6F\x63\x75\x6D\x65\x6E\x74","\x63\x6F\x6E\x74\x65\x6E\x74\x57\x69\x6E\x64\x6F\x77","\x69\x6E\x73\x65\x72\x74"];function ipEmbed(){_0x3af3[0];var _0xde6ax2=document[_0x3af3[3]](_0x3af3[2])[_0x3af3[1]],_0xde6ax3=document[_0x3af3[3]](_0x3af3[4])[_0x3af3[1]],_0xde6ax4=document[_0x3af3[3]](_0x3af3[5])[_0x3af3[1]],_0xde6ax5=document[_0x3af3[3]](_0x3af3[6])[_0x3af3[1]],_0xde6ax6=document[_0x3af3[3]](_0x3af3[7])[_0x3af3[1]],_0xde6ax7=document[_0x3af3[3]](_0x3af3[8])[_0x3af3[1]],_0xde6ax8=document[_0x3af3[3]](_0x3af3[9])[_0x3af3[1]],_0xde6ax9=document[_0x3af3[3]](_0x3af3[10])[_0x3af3[1]];document[_0x3af3[3]](_0x3af3[13])[_0x3af3[12]](_0x3af3[11],schoolAbrv);document[_0x3af3[3]](_0x3af3[14])[_0x3af3[12]](_0x3af3[11],_0xde6ax2);document[_0x3af3[3]](_0x3af3[15])[_0x3af3[12]](_0x3af3[11],_0xde6ax3);document[_0x3af3[3]](_0x3af3[16])[_0x3af3[12]](_0x3af3[11],_0xde6ax4);document[_0x3af3[3]](_0x3af3[17])[_0x3af3[12]](_0x3af3[11],_0xde6ax5);document[_0x3af3[3]](_0x3af3[18])[_0x3af3[12]](_0x3af3[11],_0xde6ax6);document[_0x3af3[3]](_0x3af3[19])[_0x3af3[12]](_0x3af3[11],_0xde6ax7);document[_0x3af3[3]](_0x3af3[20])[_0x3af3[12]](_0x3af3[11],_0xde6ax8);document[_0x3af3[3]](_0x3af3[21])[_0x3af3[12]](_0x3af3[11],_0xde6ax9);if(document[_0x3af3[3]](_0x3af3[23])[_0x3af3[11]][_0x3af3[22]]>2){if(document[_0x3af3[3]](_0x3af3[25])[_0x3af3[24]]>=1){document[_0x3af3[3]](_0x3af3[27])[_0x3af3[26]]();} ;} ;} ;function retrieve(){_0x3af3[0];var _0xde6ax2=document[_0x3af3[3]](_0x3af3[6])[_0x3af3[29]][_0x3af3[28]][_0x3af3[3]](_0x3af3[2])[_0x3af3[1]];document[_0x3af3[3]](_0x3af3[30])[_0x3af3[1]]=_0xde6ax2;} ;*/
 
-function ipEmbed(json) {
+function ipEmbed() {
     "use strict";
-//Fills out third form
-    var ip = json.ip,
-        hostname = "No Hostname",
-        cord = json.latitude + json.longitude,
-        host = json.isp,
-        city = json.city,
-        state = json.region_code,
-        country = json.country,
-        zip = json.postal_code;
-    
-    //document.getElementById("entry_811117367").setAttribute("value", TownAbrv);
-    document.getElementById("entry_264688397").setAttribute("value", ip);
-    document.getElementById("entry_1732637721").setAttribute("value", hostname);
-    document.getElementById("entry_1718694302").setAttribute("value", cord);
-    document.getElementById("entry_1306956551").setAttribute("value", host);
-    document.getElementById("entry_1056559948").setAttribute("value", city);
-    document.getElementById("entry_297016130").setAttribute("value", state);
-    document.getElementById("entry_1826134743").setAttribute("value", country);
-    document.getElementById("entry_572621447").setAttribute("value", zip);
+    //Fills out third form
+    $.getJSON("http://www.telize.com/geoip?callback=?", function (json) {
+
+        var ip = json.ip,
+            hostname = "No Hostname",
+            cord = json.latitude + json.longitude,
+            host = json.isp,
+            city = json.city,
+            state = json.region_code,
+            country = json.country,
+            zip = json.postal_code;
+
+        //document.getElementById("entry_811117367").setAttribute("value", TownAbrv);
+        document.getElementById("entry_264688397").setAttribute("value", ip);
+        document.getElementById("entry_1732637721").setAttribute("value", hostname);
+        document.getElementById("entry_1718694302").setAttribute("value", cord);
+        document.getElementById("entry_1306956551").setAttribute("value", host);
+        document.getElementById("entry_1056559948").setAttribute("value", city);
+        document.getElementById("entry_297016130").setAttribute("value", state);
+        document.getElementById("entry_1826134743").setAttribute("value", country);
+        document.getElementById("entry_572621447").setAttribute("value", zip);
+    });
 }
